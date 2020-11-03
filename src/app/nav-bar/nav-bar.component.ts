5d3ea857cf5ec8d6063b1ddb387b810dc1230d0e
Victoria de la Guardia <victoriadelaguardia@yahoo.com>
2020-11-03 17:29:32 -0600

    added shopping cart and placeholder for next section

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  @Input() cartItems = 0;
+
   ngOnInit(): void {
   }
 
